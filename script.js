2786b4d2262e17890e76cb70c719569e097173c6
Dominique McBride <ddm364@nyu.edu>
2019-12-13 15:48:35 -0500

    updated final 12/13

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 var textWrapper = document.querySelector('.hello');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({
+    loop: true
+  })
   .add({
     targets: '.hello .letter',
-    translateX: [40,0],
+    translateX: [40, 0],
     translateZ: 0,
-    opacity: [0,1],
+    opacity: [0, 1],
     duration: 10000,
     delay: (el, i) => 600 + 30 * i
   }).add({
     targets: '.hello .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
+    translateX: [0, -30],
+    opacity: [1, 0],
     duration: 1100,
     delay: (el, i) => 100 + 30 * i
   });
